1bf3714c3d9b7399eba461843447970f77f2ee4f
unknown <Administrator@MARKS-ASUS.redmond.corp.microsoft.com>
2020-03-02 21:32:23 -0800

    Added chp10, and added first two methods

---
+++ chp10/chp10.ts
@@ -0,0 +1,32 @@
+
+/*
+    - Create a deep freeze
+        for obj and for array... 
+    - Create a deepClone and not using the simply JSON.parse(JSON.stringify(obj))...
+    - Create a function for getting they so it's getAttr  = (path: Array, obj: object) => returns prop or undefined 
+    - Create a function for setting an property over the a path  so it's setByPath = (path, item, obj) => returns true if possible other wise false 
+    - Create a update object will update an object and then return a new frozen object
+*/
+
+export const deepFreeze = obj => {
+    if (typeof obj === "object" && !Object.isFrozen(obj)) {
+        const frozenObject = Object.freeze(obj);
+        Object.getOwnPropertyNames(frozenObject).forEach(prop => {
+            deepFreeze(frozenObject[prop]);
+        });
+    }
+
+    return obj;
+};
+
+export const deepClone = obj => {
+    let aux = obj;
+    if (obj && typeof obj === "object") {
+        aux = { ...obj };
+        Object.getOwnPropertyNames(prop => {
+            aux[prop] = deepClone(aux[prop]);
+        });
+    } 
+
+    return aux;
+};
