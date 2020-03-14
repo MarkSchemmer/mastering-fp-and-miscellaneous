5ee0f1a7abdcaf0e871c5fcd4bc547ec894de51d
unknown <Administrator@MARKS-ASUS.redmond.corp.microsoft.com>
2020-03-14 00:37:46 -0700

    Refactored powerShell subscriptions script 1

--- powershell/work-script/power-shell-subscriptions.ps1
+++ powershell/work-script/power-shell-subscriptions.ps1
@@ -1,17 +1,11 @@
 
 <#
 
-        [2:16 PM] Alex Triantafillidis (HCL America Inc)
-        it goes through subs and expots to csv, then save as xl
-        [2:17 PM] Alex Triantafillidis (HCL America Inc)
-        only thing left is to import the subscriptions from a csv instead directly from azure
-        [MSFT-CE-CXG-M365-CommercePurchase-INT-01.xlsx]
+    Link to azure script: https://gallery.technet.microsoft.com/scriptcenter/Export-Azure-Resource-092b9c2a
 
-        Link to azure script: https://gallery.technet.microsoft.com/scriptcenter/Export-Azure-Resource-092b9c2a
-
-#Requires -Version 5 
-#Requires -Module PackageManagement,PowerShellGet 
-#Requires -Module AzureRM.Profile,AzureRM.Resources
+    #Requires -Version 5 
+    #Requires -Module PackageManagement,PowerShellGet 
+    #Requires -Module AzureRM.Profile,AzureRM.Resources
 
 #>
 
@@ -25,26 +19,7 @@
     # Login to Azure, you will be prompted for your creds 
     Add-AzureRmAccount 
 #> 
- 
- 
-# # Loop through all Subscriptions that you have access to and export the Role information 
-# Get-AzureRmSubscription | foreach-object { 
- 
-#     Write-Verbose -Message "Changing to Subscription $($_.Name)" -Verbose 
- 
-#     Select-AzureRmSubscription -TenantId $_.TenantId -Name $_.Id -Force 
-#     $Name     = $_.Name 
-#     $TenantId = $_.TenantId 
- 
- 
-#     Get-AzureRmRoleAssignment -IncludeClassicAdministrators | Select RoleDefinitionName,DisplayName,SignInName,ObjectType,Scope, 
-#         @{name='TenantId';expression = {$TenantId}},@{name='SubscriptionName';expression = {$Name}} -OutVariable ra 
- 
-#     # Also export the individual subscriptions to excel documents on your Desktop. 
-#     # One file per subscription 
-#     $ra | Export-Csv -Path $home\Desktop\$Name.csv -NoTypeInformation 
- 
-# } 
+
  
 # once the csv files are saved on the desktop 
 # Open files, insert Table, tick 'my table has headers' 
@@ -78,16 +53,14 @@ foreach ($item in $source) {
 
     Write-Verbose -Message "Changing to Subscription $($item.Name)" -Verbose 
 
-    # why do we need line 73
     Select-AzureRmSubscription -TenantId $item.TenantId -Name $item.Id -Force  
     $Name     = $_.Name 
     $TenantId = $_.TenantId 
 
     Get-AzureRmRoleAssignment -IncludeClassicAdministrators `
     | Select-Object RoleDefinitionName, DisplayName, SignInName, ObjectType, Scope, `
-    @{name='TenantId';expression = {$TenantId}},@{name='SubscriptionName';expression = {$Name}} -OutVariable ra
-
-    $ra | ForEach-Object { $accumulator += $_ }
+        @{name='TenantId'; expression = {$TenantId}}, @{name='SubscriptionName'; expression = {$Name}} `
+    | ForEach-Object { $accumulator += $_ }
 }
 
 $accumulator | Where-Object { filterRoleDefinitionName $_.RoleDefinitionName } | Export-Csv -Path $fileDestinationOutPut
