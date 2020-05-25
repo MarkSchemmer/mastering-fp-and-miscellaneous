af0d9bea2a4c9e517b10241aa506d64675b106d9
unknown <Administrator@MARKS-ASUS.redmond.corp.microsoft.com>
2020-05-25 01:19:37 -0700

    Added problem 155

--- leetCode-Problems/problems.ts
+++ leetCode-Problems/problems.ts
@@ -1377,3 +1377,18 @@ var hasCycle = function(head) {
     
     return false;
 };
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit2 = prices => {
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) 
+            maxProfit = prices[i] - prices[i - 1];
+    }
+
+    return maxProfit;
+};
