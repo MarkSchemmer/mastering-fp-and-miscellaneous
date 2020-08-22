ec7731eb90fc68906b4fbce8c1e69ca5bcf8d7be
unknown <Administrator@MARKS-ASUS.redmond.corp.microsoft.com>
2020-08-21 21:38:44 -0700

    Added codibility question

--- C#DesignPatterns/Program.cs
+++ C#DesignPatterns/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace C_DesignPatterns
 {
@@ -9,15 +10,17 @@ namespace C_DesignPatterns
             var test = new Addor();
             var test2 = new Addor();
 
-            Console.WriteLine(test.add());
+            // Console.WriteLine(test.add());
 
-            Console.WriteLine(
-                Object.ReferenceEquals(test, test2)
-            );
+            // Console.WriteLine(
+            //     Object.ReferenceEquals(test, test2)
+            // );
 
-            Console.WriteLine(
-                Object.ReferenceEquals(test.getInstance(), test2.getInstance())
-            );
+            // Console.WriteLine(
+            //     Object.ReferenceEquals(test.getInstance(), test2.getInstance())
+            // );
+
+            // Console.WriteLine(default(int));
 
 
             // Addor1 tests 
@@ -27,9 +30,101 @@ namespace C_DesignPatterns
 
             // Proves singletong works... 
             var finalTest = Object.ReferenceEquals(addor1Test2.getInstance(), addor1Test3.getInstance());
-            Console.WriteLine(finalTest);
+            // Console.WriteLine(finalTest);
             // Addor1 type of checking... 
+
+            var valArgs = new ValidateArgs();
+            string[] args1 = new string[]{ "--count", "name"};
+            var res = valArgs.Validate(args1);
+            Console.WriteLine(
+                res
+            );
+
+
+            string[] args2 = new string[] { "--name" };
+
+            Console.WriteLine(
+                valArgs.Validate(args2)
+            );
+
             Console.WriteLine("Hello World!");
+        }
+
+
+
+
+        public class ValidateArgs 
+        {
+
+            public bool isValidString (string s) 
+            {
+                if(s[0] == '-' && s[1] == '1') return true;
+                return s.Length >= 3 && s.Length <= 10;
+            }
+
+            public bool isValidNum (int n) 
+            {
+                return n >= 10 && n <= 100;
+            }
+
+
+            public int Validate(string[] args)
+            {
+                args.ToList().ForEach(x => Console.WriteLine(x));
+                var help = "help";
+                var name = "--name";
+                var count = "--count";
+
+                var doesContainHelp = false;
+                if (args.Length == 0) return -1;
+                var ListArgs = args.Select(x => x.ToLower()).ToList();
+                // Need to find errors
+                string nextShouldBe = null;
+
+                for (int i = 0; i < ListArgs.Count; i++) {
+
+                    var val = ListArgs[i];
+                    if(val.Contains(help)) { doesContainHelp = true; continue; }
+
+                    // Command step
+                    if (nextShouldBe == null && val == name) {
+                        nextShouldBe = name;
+                        continue;
+                    } else if (nextShouldBe == null && val == count) {
+                        Console.WriteLine("I'm here.");
+                        nextShouldBe = count;
+                        continue;
+                    } else if(nextShouldBe == null) {
+                        return -1;
+                    }
+
+                    // test step
+
+                    if(nextShouldBe == count) {
+                        var canConvert = int.TryParse(val, out int num);
+                        Console.WriteLine(canConvert);
+                        if (canConvert == true) {
+                            if (!isValidNum(num)) return -1;
+
+                            
+                            nextShouldBe = null;
+                            continue;
+                        } else {
+                            return -1;
+                        }
+                    }
+
+
+                    if (nextShouldBe == name) {
+                        if(!isValidString(val)) return -1;
+
+                        nextShouldBe = null;
+                        continue;
+                    }
+                }
+                if (nextShouldBe != null && doesContainHelp == false) return -1;
+                return doesContainHelp ? 1 : 0;
+            }
         }
 
 
